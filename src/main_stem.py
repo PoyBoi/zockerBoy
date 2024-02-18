642c7d5c7fb7fbfbf8ce20fa6a9c79bb14dd892b
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-02-18 17:15:44 +0530

    edits

--- src/main_stem.py
+++ src/main_stem.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+# from rudimentary_func import download_file
 
 # image_path = r"C:\Users\parvs\OneDrive\Pictures\Cyberpunk 2077\photomode_01032023_011806.png"
 
@@ -18,17 +19,17 @@ def get_color_palette(img_path = r"zockerBoy\image\test_ad.jpg"):
 #__main__
 # print(get_color_palette(image_path))
     
-def get_text_overlay(img_path = r"C:\Users\parvs\VSC Codes\Python-root\zockerBoy\image\test_ad.jpg"):
+def get_text_overlay(img_path:str = r"C:\Users\parvs\VSC Codes\Python-root\zockerBoy\image\test_ad.jpg") -> str:
   from PIL import Image
   import pytesseract
+  # I could have used CNN's here but that wouldn't be that good compared to this
 
   # Open the image file
   img = Image.open(img_path)
   pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-  #!TODO need to find the tesseract location and then assign it to this variable
-  # Use pytesseract to convert the image into text
+  # Need a way to install tesseract.exez
   text = pytesseract.image_to_string(img)
   return text
 
 #__main__
-print(get_text_overlay())
+print(get_text_overlay(r"zockerBoy\image\blinkit.png"))
