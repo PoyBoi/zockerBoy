a33b849963abd52d1368d9340a3a0348e8262dd6
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-02-21 23:21:18 +0530

    added bw to OCR, init obj_det

--- src/main_stem.py
+++ src/main_stem.py
@@ -4,6 +4,9 @@ from PIL import Image
 from pytesseract import Output
 import matplotlib.pyplot as plt
 from colorthief import ColorThief
+from transformers import AutoModelForCausalLM, CodeGenTokenizerFast as Tokenizer
+# https://pytorch.org/get-started/locally/
+
 # from rudimentary_func import download_file
 
 def get_color_palette(img_path:str = r"zockerBoy\image\test_ad.jpg") -> list:
@@ -36,7 +39,12 @@ def get_overlay_box(img_path:str = r"C:\Users\parvs\VSC Codes\Python-root\zocker
   # text_ext = pytesseract.image_to_string(Image.open(img_path), config=myconfig)
   img_box = cv2.imread(img_path)
   # h, w, _ = img_box.shape
+  gray_image = cv2.cvtColor(img_box, cv2.COLOR_BGR2GRAY)
 
+  # ret, bw_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+  # img_box = bw_image
+  
+  img_box = gray_image
   out_txt = ""
   data = pytesseract.image_to_data(img_box, config=myconfig, output_type=Output.DICT)
   for i in range(len(data['text'])):
@@ -50,9 +58,28 @@ def get_overlay_box(img_path:str = r"C:\Users\parvs\VSC Codes\Python-root\zocker
         out_txt += x + " "
   print(out_txt)
 
-#__main__
+def obj_det(qst:str, img_path:str = r"C:\Users\parvs\VSC Codes\Python-root\zockerBoy\image\test_ad.jpg") -> str:
+  # can use YOLO or moondream and maybe layer it with openCV
+  # This does not work because vit_so400m_14_siglip_384 does not want to work
 
+  model_id = "vikhyatk/moondream1"
+  model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
+  tokenizer = Tokenizer.from_pretrained(model_id)
+
+  img = Image.open(img_path)
+  enc_image = model.encode_image(img)
+  print(model.answer_question(enc_image, qst, tokenizer))
+
+def object_detection(img_path:str = r"C:\Users\parvs\VSC Codes\Python-root\zockerBoy\image\test_ad.jpg") -> str:
+  None
+
+#__main__
+  
+from models\bulding\mmdet\mmdetection\mmdet\apis import Det
+  
 # print(get_color_palette(r"zockerBoy\image\test_ad.jpg"))
 # print(get_text_overlay(r"zockerBoy\image\test_ad.jpg"))
 # print(get_text_overlay(r"C:\Users\parvs\Downloads\Fwaut2PaEAQynD4.jpg"))
 # get_overlay_box(r"C:\Users\parvs\Downloads\Fwaut2PaEAQynD4.jpg")
+# get_overlay_box()
+# obj_det("What is the object in the image ?")
