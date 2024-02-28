d827b9307c6c363a8575e9ddb2d10840d45b5142
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-02-28 05:56:12 +0530

    added run_det to elevated

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .main_stem import (get_overlay_box, object_detection, get_color_palette)
-# from logo_det import run_det
+from .logo_det import run_det
 
 __all__ = [
-    'get_overlay_box', 'object_detection', 'get_color_palette'
+    'get_overlay_box', 'object_detection', 'get_color_palette', 'run_det'
 ]
