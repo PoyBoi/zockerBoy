b6d1edcebba4083506b2a3065d2caeaa1f3cb010
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-02-28 05:56:26 +0530

    init basic path

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 from zockerBoy.src.main_stem import (get_color_palette, get_overlay_box, object_detection)
+from zockerBoy.src.logo_det import run_det
 
+isAd = 3 # Will change to 4 once logo is added
+img_path = r'C:\\Users\\parvs\\Downloads\\games_cyberpunk_posters.webp'
+
+getPalette = get_color_palette(img_path)
+textOCR = get_overlay_box(img_path)
+objDetection = run_det(img_path, 1)
