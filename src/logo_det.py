203bb3d65f4ae839371d4b83b9387ce89379f64a
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-02-28 05:56:50 +0530

    removed direct file bound, function now accessible

--- src/logo_det.py
+++ src/logo_det.py
@@ -20,8 +20,9 @@ args = parser.parse_args()
 
 names = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle', 4: 'airplane', 5: 'bus', 6: 'train', 7: 'truck', 8: 'boat', 9: 'traffic light', 10: 'fire hydrant', 11: 'stop sign', 12: 'parking meter', 13: 'bench', 14: 'bird', 15: 'cat', 16: 'dog', 17: 'horse', 18: 'sheep', 19: 'cow', 20: 'elephant', 21: 'bear', 22: 'zebra', 23: 'giraffe', 24: 'backpack', 25: 'umbrella', 26: 'handbag', 27: 'tie', 28: 'suitcase', 29: 'frisbee', 30: 'skis', 31: 'snowboard', 32: 'sports ball', 33: 'kite', 34: 'baseball bat', 35: 'baseball glove', 36: 'skateboard', 37: 'surfboard', 38: 'tennis racket', 39: 'bottle', 40: 'wine glass', 41: 'cup', 42: 'fork', 43: 'knife', 44: 'spoon', 45: 'bowl', 46: 'banana', 47: 'apple', 48: 'sandwich', 49: 'orange', 50: 'broccoli', 51: 'carrot', 52: 'hot dog', 53: 'pizza', 54: 'donut', 55: 'cake', 56: 'chair', 57: 'couch', 58: 'potted plant', 59: 'bed', 60: 'dining table', 61: 'toilet', 62: 'tv', 63: 'laptop', 64: 'mouse', 65: 'remote', 66: 'keyboard', 67: 'cell phone', 68: 'microwave', 69: 'oven', 70: 'toaster', 71: 'sink', 72: 'refrigerator', 73: 'book', 74: 'clock', 75: 'vase', 76: 'scissors', 77: 'teddy bear', 78: 'hair drier', 79: 'toothbrush'}
 
-if __name__ == '__main__':    
-    def run_det(img_path:str = r"C:\Users\parvs\VSC Codes\Python-root\zockerBoy\image\test_ad.jpg", dev:int = 0) -> str:
+#if __name__ == '__main__':    
+def run_det(img_path:str = r"C:\Users\parvs\VSC Codes\Python-root\zockerBoy\image\test_ad.jpg", dev:int = 0) -> str:
+    try:
         model = YOLO("yolov8m.yaml")  # build a new model from scratch
         model = YOLO(r"zockerBoy\models\yolov8m.pt")  # load a pretrained model (recommended for training)
         output = []
@@ -47,11 +48,15 @@ if __name__ == '__main__':
             for i in boxes.cls:
                 op = str(i).split("(")[1].split(".")[0]
                 output.append(names[int(op)])
-
+        # print(output)
         return output
+    except Exception as e:
+        print("Encountered error ", e)
 
 if args.i == None or args.d == None:
-    print("Incorrect / Not enough variables supplied")
+    # print("Incorrect / Not enough variables supplied")
+    # print("Loading")
+    None
 else:
     run_det(args.i, args.d)
             
