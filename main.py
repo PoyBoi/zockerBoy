fb5f2cab010e884ebf164331cbb39f7067aa3331
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-02-28 06:31:04 +0530

    added clear terminal before output

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from zockerBoy.src.main_stem import (get_color_palette, get_overlay_box, object_detection)
 from zockerBoy.src.logo_det import run_det
+from webcolors import rgb_to_name
+import os
 
 isAd = 3 # Will change to 4 once logo is added
 img_path = r'C:\\Users\\parvs\\Downloads\\games_cyberpunk_posters.webp'
@@ -8,6 +10,11 @@ getPalette = get_color_palette(img_path)
 textOCR = get_overlay_box(img_path)
 objDetection = run_det(img_path, 1)
 
+if os.name == 'nt':  # Windows
+    os.system('cls')
+else:  # Unix-like systems (macOS, Linux)
+    os.system('clear') 
+
 print(
-    "Color pallete is: {}, \n Overlayed text says: {}, \n, Objects detected are: {}".format(getPalette, textOCR, objDetection)
+    "Color pallete is: {}, \n Overlayed text says: {}, \n Objects detected are: {}".format(getPalette, textOCR, objDetection)
 )
