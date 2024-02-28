5cebc1e76b730bdcd7aa902381b3ea2c4c0478ae
PoyBoi <106402089+PoyBoi@users.noreply.github.com>
2024-02-28 05:40:36 +0530

    removed run_det

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from .main_stem import (get_overlay_box, object_detection, get_color_palette)
+# from logo_det import run_det
 
 __all__ = [
     'get_overlay_box', 'object_detection', 'get_color_palette'
